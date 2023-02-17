b8ac514411db904cec971a190370bdb85cb279ba
Grace Henry <henryn98@uw.edu>
2023-02-17 14:11:14 -0800

    commit

--- analysis/assignment-5.R
+++ analysis/assignment-5.R
@@ -8,27 +8,35 @@ plot(
   x=siscowet$len,
   y=siscowet$wgt
 )
-abline(lm(siscowet$wgt ~ siscowet$len))
+abline(a=mean(siscowet$wgt, na.rm=TRUE), b=0)
 
 plot(
   x=siscowet[siscowet$locID == "Deer Park", "len"],
   y=siscowet[siscowet$locID == "Deer Park", "wgt"]
 )
+abline(a=mean(siscowet[siscowet$locID == "Deer Park", "wgt"],
+              na.rm=TRUE), b=0)
 
 plot(
   x=siscowet[siscowet$locID == "Blind Sucker", "len"],
   y=siscowet[siscowet$locID == "Blind Sucker", "wgt"]
 )
+abline(a=mean(siscowet[siscowet$locID == "Blind Sucker", "wgt"],
+              na.rm=TRUE), b=0)
 
 plot(
   x=siscowet[siscowet$locID == "Grand Marais", "len"],
   y=siscowet[siscowet$locID == "Grand Marais", "wgt"]
 )
+abline(a=mean(siscowet[siscowet$locID == "Grand Marais", "wgt"],
+              na.rm=TRUE), b=0)
 
 plot(
   x=siscowet[siscowet$locID == "Little Lake Harbor", "len"],
   y=siscowet[siscowet$locID == "Little Lake Harbor", "wgt"]
 )
+abline(a=mean(siscowet[siscowet$locID == "Little Lake Harbor", "wgt"],
+              na.rm=TRUE), b=0)
 
 #Since one of the Little Lake Harbor values is an obvious outlier
 #I am looking for where that is 
@@ -38,5 +46,15 @@ siscowet[siscowet$locID == "Little Lake Harbor", "wgt"]
 #was entered as 15800 instead of 1580 so I am replacing that value 
 siscowet[541,"wgt"] <- 1580
 
+#Expository figure 
+
+plot(
+  x=siscowet[siscowet$locID == "Deer Park", "len"],
+  y=siscowet[siscowet$locID == "Deer Park", "wgt"]
+)
+abline(a=mean(siscowet[siscowet$locID == "Deer Park", "wgt"],
+              na.rm=TRUE), b=0)
+text(x = 300, y = 2000, 
+     floor(mean(siscowet[siscowet$locID == "Deer Park", "wgt"])))
 
 
